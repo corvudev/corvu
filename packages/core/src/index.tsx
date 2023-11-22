a766b3d8075a2b112f5b6738feb5c22594f87c08
Jasmin <code@jasi.dev>
2023-11-23 00:02:08 +0100

    feat!: remove createOnce export

--- packages/core/src/index.tsx
+++ packages/core/src/index.tsx
@@ -3,7 +3,6 @@ export { default as Polymorphic, As } from '@lib/components/Polymorphic'
 export { default as createControllableSignal } from '@lib/create/controllableSignal'
 export { default as createDisableScroll } from '@lib/create/disableScroll'
 export { default as createFocusTrap } from '@lib/create/focusTrap'
-export { default as createOnce } from '@lib/create/once'
 export { default as createPresence } from '@lib/create/presence'
 
 export type {
