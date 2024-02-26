a6437c9434d3d6520cea2008f71d631a694473ac
Jasmin <code@jasi.dev>
2024-02-26 16:21:32 +0100

    docs: update example tailwind configs

--- web/src/examples/primitives/accordion/tailwind.js
+++ web/src/examples/primitives/accordion/tailwind.js
@@ -5,20 +5,11 @@ export default {
     extend: {
       colors: {
         corvu: {
-          50: '#f2f0fe',
+          bg: '#f3f1fe',
           100: '#e6e2fd',
           200: '#d4cbfb',
-          light: '#D4C0FF',
           300: '#bcacf6',
           400: '#a888f1',
-          500: '#9a6de9',
-          600: '#8f50dc',
-          700: '#7e41c3',
-          accent: '#7250AE',
-          800: '#63359c',
-          900: '#52317d',
-          dark: '#180f23',
-          1000: '#0C0812',
         },
       },
       animation: {
