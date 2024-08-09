a2971c0686be02f58341e33907a09f06b4dc4efc
Jasmin <code@jasi.dev>
2024-08-09 15:57:08 +0200

    docs: fix mobile nav header shrinking

--- web/src/components/Drawer.tsx
+++ web/src/components/Drawer.tsx
@@ -41,7 +41,10 @@ const NavDrawer: FlowComponent = (props) => {
               }}
             />
             <Drawer.Content class="fixed inset-y-0 left-0 z-50 flex w-[230px] flex-col items-start bg-corvu-bg after:absolute after:inset-y-0 after:right-[calc(100%-1px)] after:w-1/2 after:bg-inherit corvu-transitioning:transition-transform corvu-transitioning:duration-500 corvu-transitioning:ease-[cubic-bezier(0.32,0.72,0,1)]">
-              <a href="/" class="flex h-[72px] items-center px-3">
+              <a
+                href="/"
+                class="flex h-[72px] w-full shrink-0 items-center rounded-bl-xl border-b-4 border-corvu-200 px-3"
+              >
                 <span class="sr-only">corvu</span>
                 <img
                   src={HeaderLogoDark.src}
